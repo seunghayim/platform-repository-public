d01db2f97eb47e83c41886f350a75d63830b91b4
seunghayim <ha@DESKTOP-7OULA8F.localdomain>
2022-05-24 18:36:35 +0900

    Update .gitignore

--- bastion-host.tf
+++ bastion-host.tf
@@ -32,7 +32,7 @@ resource "null_resource" "provisioner" {
   # Changes to any instance of the cluster requires re-provisioning
   triggers = {
     instance_id = aws_instance.provisioner.id
-    script      = filemd5("${path.module}/files/kubectl-eksctl-cli.sh")
+    script      = filemd5("${path.module}/files/kubectl-eksctl.sh")
   }
 
   # Bootstrap script can run on any instance of the cluster
@@ -40,7 +40,7 @@ resource "null_resource" "provisioner" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("${path.module}/files/.ssh/id_rsa")
+    private_key = file("${path.module}/files/.pem/id_rsa")
     host        = aws_instance.provisioner.public_ip
   }
 
@@ -49,20 +49,14 @@ resource "null_resource" "provisioner" {
     destination = "/tmp"
   }
 
-  provisioner "file" {
-    source      = "${path.module}/files/.ssh"
-    destination = "/tmp"
-  }
-
   provisioner "remote-exec" {
     inline = [
       "sudo mv /tmp/.aws ~/",
-      "sudo mv /tmp/.ssh ~/"
     ]
   }
 
   provisioner "remote-exec" {
-    script = "${path.module}/files/kubectl-eksctl-cli.sh"
+    script = "${path.module}/files/kubectl-eksctl.sh"
   }
 
   depends_on = [
--- files/kubectl-eksctl.sh
+++ files/kubectl-eksctl.sh
@@ -19,6 +19,6 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-# kubectl update
+# kubectl update !
 aws eks update-kubeconfig --region ap-southeast-2 --name multi05-eks-cluster-terraform
 
